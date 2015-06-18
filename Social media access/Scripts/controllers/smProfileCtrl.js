88686c7ba96ee451607b54c34026c392ffad4135
simbu <simbuec2005@gmail.com>
2015-06-19 00:42:35 +0530

    Profile almost done.

--- Social media access/Scripts/controllers/smProfileCtrl.js
+++ Social media access/Scripts/controllers/smProfileCtrl.js
@@ -1,12 +1,29 @@
 smApp.controller('smProfileCtrl', ['$scope', 'smProfileApi', function ($scope, smProfileApi) {
     var user = userInfo;
     var lastTweetId = LastTweetId;
+    var lastProfileTweetId;
     $scope.newPost = {};
-    console.log(user);
-    smProfileApi.getHomeTweets(user).then(function (data) {
-        $scope.tweets = data;
-    }, function () {
-    });
+    $scope.myProfile = true;
+     
+    /* get tweets and mentions of logged in user*/
+    var onInit = function () {
+        $scope.showBackDrop = true;
+        smProfileApi.getHomeTweets(user).then(function (data) {
+            $scope.tweets = data;
+            $scope.showBackDrop = false;
+        }, function () {
+            $scope.showBackDrop = false;
+        });
+
+        smProfileApi.getMentions(user).then(function (data) {
+            $scope.mentions = data;
+        }, function () {
+            console.log('Unable to complete the request');
+        });
+    }
+    onInit();
+    
+
     $scope.retweet = function (tweetId) {
         var payload = angular.extend({id: tweetId},user)
         smProfileApi.reTweet(payload).then(function () {
@@ -14,6 +31,7 @@ smApp.controller('smProfileCtrl', ['$scope', 'smProfileApi', function ($scope, s
         }, function () { });
     }
 
+    /* comment on tweets */
     $scope.reply = function (tweet) {
         var payload = {
             statusId: tweet.Id,
@@ -26,15 +44,17 @@ smApp.controller('smProfileCtrl', ['$scope', 'smProfileApi', function ($scope, s
             console.log('success');
         }, function () { });
     }
-
+    /* delete tweets */
     $scope.deleteTweet = function (tweetId) {
         var payload = angular.extend({ id: tweetId }, user);
+
         smProfileApi.deleteTweet(payload).then(function () {
             console.log('success');
         }, function () { });
 
     }
     
+    /* load more logged in user tweets */
     $scope.loadMoreTweets = function () {
         var payload = angular.extend({ id: lastTweetId }, user);
         $scope.showLoader = true;
@@ -47,6 +67,7 @@ smApp.controller('smProfileCtrl', ['$scope', 'smProfileApi', function ($scope, s
             $scope.showLoader = false;
         });
     }
+    /* post new tweet */
     $scope.updateStatus = function (status) {
         var payload = angular.extend({ statusText: status }, user);
         $scope.disableStatusBox = true;
@@ -55,6 +76,91 @@ smApp.controller('smProfileCtrl', ['$scope', 'smProfileApi', function ($scope, s
             $scope.disableStatusBox = false;
         }, function (error) {
             console.log('Unable to post currently');
+        });
+    }
+
+    /* utility method */
+    function isEmptyObject(obj) {
+        var key;
+        for (key in obj) {
+            return false;
+        }
+        return true;
+    }
+
+    /* get tweets of searched profile*/
+    $scope.getTweets = function (screenName) {
+        $scope.showBackDrop = true;
+        var payload = angular.extend({ screenName: screenName }, user);
+
+        smProfileApi.getBasicInfo(payload).then(function (screenInfo) {
+            if (!isEmptyObject(screenInfo)) {
+                $scope.myProfile = false;
+                $scope.profile = screenInfo;
+                smProfileApi.getTweets(payload).then(function (data) {
+                    lastProfileTweetId = data[data.length - 1] && (data[data.length - 1]).Id;
+                    $scope.tweets = data;
+                    $scope.hideStatusBox = true;
+                    $scope.hideHeader = false;
+                    $scope.showBackDrop = false;
+                }, function (error) {
+                    $scope.showBackDrop = false;
+                    console.log('Unable to get your tweets');
+                });
+            }
+        }, function () {
+            $scope.showBackDrop = false;
+            console.log('Unable to get screen information');
+        });
+    }
+    /* Follow searched profile*/
+    $scope.follow = function (screenName) {
+        var payload = angular.extend({ screenName: screenName }, user);
+
+        smProfileApi.follow(payload).then(function (screenInfo) {
+            console.log('following..');
+        }, function () {
+            console.log('Unable to complete the request');
+        });
+    }
+    /* Unfollow searched profile*/
+    $scope.unfollow = function (screenName) {
+        var payload = angular.extend({ screenName: screenName }, user);
+
+        smProfileApi.unfollow(payload).then(function (screenInfo) {
+            console.log('unfollowing..');
+        }, function () {
+            console.log('Unable to complete the request');
+        });
+    }
+    /* Search in all (People, Page, Content) */
+    $scope.search = function (searchStr) {
+        $scope.showBackDrop = true;
+        var payload = angular.extend({ term: searchStr }, user);
+        $scope.searchStr = searchStr;
+        smProfileApi.search(payload).then(function (data) {
+            $scope.myProfile = false;
+            $scope.hideHeader = true;
+            $scope.hideStatusBox = true;
+            $scope.tweets = data.Statuses;
+            $scope.showBackDrop = false;
+        }, function () {
+            $scope.showBackDrop = false;
+            console.log('Unable to complete the request');
+        });
+    }
+    
+    /* load more of searched profile*/
+    $scope.loadMoreProfileTweets = function (screenName) {
+        var payload = angular.extend({ id: lastProfileTweetId, screenName: screenName }, user);
+        $scope.showLoader = true;
+        smProfileApi.loadMoreProfileTweets(payload).then(function (data) {
+            lastProfileTweetId = data[data.length - 1] && (data[data.length - 1]).Id;
+            $scope.tweets = $scope.tweets.concat(data);
+            $scope.showLoader = false;
+        }, function (error) {
+            console.log(error);
+            $scope.showLoader = false;
         });
     }
     
--- Social media access/Scripts/services/smProfileService.js
+++ Social media access/Scripts/services/smProfileService.js
@@ -10,8 +10,16 @@ smApp.service('smProfileApi', ['$http', function ($http) {
         replyUrl = "/Twitter/ReplyTweet",
         deleteTweetUrl = "/Twitter/Delete",
         loadMoreTweetUrl = "/Twitter/GetHomeTweetsPrev",
-        updateStatusUrl = "/Twitter/UpdateStatus";
+        updateStatusUrl = "/Twitter/UpdateStatus",
+        getTweetsUrl = "/Twitter/GetTweets",
+        getBasicInfoUrl = "/Twitter/GetBasicInfo",
+        followUrl = "/Twitter/Follow",
+        unfollowUrl = "/Twitter/UnFollow",
+        searchUrl = "/Twitter/Search",
+        getMentionsUrl = "/Twitter/GetMentions",
+        loadMoreProfileTweetsUrl = "/Twitter/GetTweetsPrev";
 
+    /* Get tweets of logged in user */
     this.getHomeTweets = function (payload) {
         return $http.get(getTweetsUrl, { params: payload }).then(success, fail);
     }
@@ -21,11 +29,43 @@ smApp.service('smProfileApi', ['$http', function ($http) {
     this.deleteTweet = function (payload) {
         return $http.get(deleteTweetUrl, { params: payload }).then(success, fail);
     }
+    /* load more tweets of logged in user */
     this.loadMoreTweet = function (payload) {
         return $http.get(loadMoreTweetUrl, { params: payload }).then(success, fail);
     }
+    /* Post New Tweet */
     this.updateStatus = function (payload) {
         return $http.post(updateStatusUrl, payload).then(success, fail);
     }
+    /* Get basic information of the searched profile */
+    this.getBasicInfo = function (payload) {
+        return $http.get(getBasicInfoUrl, { params: payload }).then(success, fail);
+    }
+    /* Get tweets of the searched profile */
+    this.getTweets = function (payload) {
+        return $http.get(getTweetsUrl, { params: payload }).then(success, fail);
+    }
+    /* Follow searched profile */
+    this.follow = function (payload) {
+        return $http.get(followUrl, { params: payload }).then(success, fail);
+    }
+    /* Unfollow searched profile */
+    this.unfollow = function (payload) {
+        return $http.get(unfollowUrl, { params: payload }).then(success, fail);
+    }
+    /* Search in all (People, Page, Content) */
+    this.search = function (payload) {
+        return $http.get(searchUrl, { params: payload }).then(success, fail);
+    }
+    /* Get mentions of the logged in user */
+    this.getMentions = function (payload) {
+        return $http.get(getMentionsUrl, { params: payload }).then(success, fail);
+    }
+    /* load more tweets of searched profile */
+    this.loadMoreProfileTweets = function (payload) {
+        return $http.get(loadMoreProfileTweetsUrl, { params: payload }).then(success, fail);
+    }
+
+    
 
 }]);
